c2814d3fb2d998999d01628001d01c333c911ce2
wait4ndbleed <73363131+wait4ndbleed@users.noreply.github.com>
2023-09-02 17:24:25 -0300

    Update PostContext.tsx
    Adicionando useEffect para guardar os dados do array posts no localStorage

--- src/contexts/PostContext.tsx
+++ src/contexts/PostContext.tsx
@@ -1,6 +1,8 @@
 import { PostActions, postReducer } from "@/reducers/postReducer";
 import { Post } from "@/types/Post";
-import { Dispatch, ReactNode, createContext, useContext, useReducer } from "react";
+import { Dispatch, ReactNode, createContext, useContext, useEffect, useReducer } from "react";
+
+const STORAGE_KEY = 'postContextContent'
 
 type PostContextType = {
     posts: Post[];
@@ -10,7 +12,14 @@ type PostContextType = {
 export const PostContext = createContext<PostContextType | null>(null);
 
 export const PostProvider = ({ children } : {children: ReactNode}) => {
-    const [posts, dispatch] = useReducer(postReducer, []);
+    const [posts, dispatch] = useReducer(
+        postReducer,
+        JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+        );
+
+    useEffect(() => {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(posts));
+    }, [posts]);
 
     return (
         <PostContext.Provider value={{ posts, dispatch }}>{children}</PostContext.Provider>
